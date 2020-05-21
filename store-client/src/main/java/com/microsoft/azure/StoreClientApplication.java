c12dd1a1da6a85c7a2d376517b5669d08df23d80
Ashish Chhabria <aschhab@microsoft.com>
2020-05-21 14:16:44 -0700

    initiating a store client

---
+++ store-client/src/main/java/com/microsoft/azure/StoreClientApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreClientApplication.class, args);
+	}
+
+}
