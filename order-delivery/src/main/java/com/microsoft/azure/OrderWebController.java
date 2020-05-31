a1d7c8b35f1b3b1b541c08670df271bb003abe9f
Ashish Chhabria <aschhab@microsoft.com>
2020-05-31 00:30:05 -0700

    starting order web controller, still need to fix order state display

---
+++ order-delivery/src/main/java/com/microsoft/azure/OrderWebController.java
@@ -0,0 +1,71 @@
+package com.microsoft.azure;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.microsoft.azure.models.common.Order;
+import com.microsoft.azure.service.OrderService;
+
+@Controller
+public class OrderWebController {
+	
+	@Autowired
+	private Topic topic;
+	
+	@Autowired
+	private Queue queue;
+	
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	@Autowired
+	private OrderService orderService;
+
+	// ----------- Order form -----------------//
+	@GetMapping("/orderadd")
+	public String orderForm(Model model) {
+		model.addAttribute("order", new Order());
+		return "orderform";
+	}
+	
+	// ---------- Create Order ----------------//
+	@PostMapping("/orderadd")
+	public String orderSubmit(@ModelAttribute Order order) {
+		
+		orderService.saveOrder(order);
+		jmsTemplate.convertAndSend(topic, order, new MessagePostProcessor() {
+			
+			@Override
+			public Message postProcessMessage(Message message) throws JMSException {
+				message.setStringProperty("address", order.getAddress());
+				message.setJMSReplyTo(queue);
+				return message;
+			}
+		});
+		return "orderView";
+	}
+	
+	// ------------- View order -------------------//
+	@GetMapping("/orderview/{id}")
+	public String orderView(@PathVariable long id, Model model) {
+		Order order = orderService.findById(id);
+		if (order == null) {
+			return "orderNotFound";
+		}
+		
+		model.addAttribute("order", order);
+		return "orderView";
+	}
+}
