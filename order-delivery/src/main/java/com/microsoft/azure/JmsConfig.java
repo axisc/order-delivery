a7ca44701aa356cfe8ac86b6f478d70c396380c0
Ashish Chhabria <aschhab@microsoft.com>
2020-06-01 09:29:47 -0700

    cleaning up comments and unused imports

--- order-delivery/src/main/java/com/microsoft/azure/JmsConfig.java
+++ order-delivery/src/main/java/com/microsoft/azure/JmsConfig.java
@@ -3,12 +3,9 @@ package com.microsoft.azure;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Queue;
-import javax.jms.TemporaryQueue;
 import javax.jms.Topic;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.command.ActiveMQTempQueue;
-import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,17 +37,6 @@ public class JmsConfig {
 				return RESPONSE_QUEUE;
 			}
 		};
-//		return new TemporaryQueue() {
-//			
-//			@Override
-//			public String getQueueName() throws JMSException {
-//				return RESPONSE_QUEUE;
-//			}
-//			
-//			@Override
-//			public void delete() throws JMSException {
-//			}
-//		};
 	}
 	
 	@Bean
--- store-client/src/main/java/com/microsoft/azure/JmsConfig.java
+++ store-client/src/main/java/com/microsoft/azure/JmsConfig.java
@@ -3,11 +3,8 @@ package com.microsoft.azure;
 import javax.jms.ConnectionFactory;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
 
 import com.microsoft.azure.service.StoreOrderService;
 import com.microsoft.azure.service.StoreOrderServiceImpl;
